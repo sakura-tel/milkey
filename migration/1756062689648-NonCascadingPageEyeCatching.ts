41f6ddebf090486d00f08a4e051ba3abfb199242
atsu1125 <atsuchan@atsuchan.page>
2025-12-17 22:03:40 +0900

    fix: Pages will be deleted when eye-catching image is removed
    Co-Authored-By: anatawa12 <anatawa12@users.noreply.github.com>

---
+++ migration/1756062689648-NonCascadingPageEyeCatching.ts
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class NonCascadingPageEyeCatching1756062689648 implements MigrationInterface {
+    name = 'NonCascadingPageEyeCatching1756062689648'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "page" ADD CONSTRAINT "FK_a9ca79ad939bf06066b81c9d3aa" FOREIGN KEY ("eyeCatchingImageId") REFERENCES "drive_file"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "page" DROP CONSTRAINT "FK_a9ca79ad939bf06066b81c9d3aa"`);
+    }
+}
