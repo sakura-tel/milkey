3ca1a1aee0293766a58247fdb84ae72a04bd207f
atsu1125 <atsuchan@atsuchan.page>
2025-12-18 13:15:41 +0900

    fixup migration NonCascadingPageEyeCatching1756062689648 for groundpolis

--- migration/1756062689648-NonCascadingPageEyeCatching.ts
+++ migration/1756062689648-NonCascadingPageEyeCatching.ts
@@ -8,10 +8,12 @@ export class NonCascadingPageEyeCatching1756062689648 implements MigrationInterf
     name = 'NonCascadingPageEyeCatching1756062689648'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "page" DROP CONSTRAINT "FK_3126dd7c502c9e4d7597ef7ef10"`);
         await queryRunner.query(`ALTER TABLE "page" ADD CONSTRAINT "FK_a9ca79ad939bf06066b81c9d3aa" FOREIGN KEY ("eyeCatchingImageId") REFERENCES "drive_file"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "page" DROP CONSTRAINT "FK_a9ca79ad939bf06066b81c9d3aa"`);
+        await queryRunner.query(`ALTER TABLE "page" ADD CONSTRAINT "FK_3126dd7c502c9e4d7597ef7ef10" FOREIGN KEY ("eyeCatchingImageId") REFERENCES "drive_file"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 }
