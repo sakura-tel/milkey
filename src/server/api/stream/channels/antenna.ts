e28af768fd8aff2305f9e0fee64d6868bd6597b3
atsu1125 <atsuchan@atsuchan.page>
2026-03-10 16:03:42 +0900

    fix(backend): antenna ownership check for antenna channels

--- src/server/api/stream/channels/antenna.ts
+++ src/server/api/stream/channels/antenna.ts
@@ -1,17 +1,27 @@
 import autobind from 'autobind-decorator';
 import Channel from '../channel';
-import { Notes } from '../../../../models';
+import { Notes, Antennas } from '../../../../models';
 import { isMutedUserRelated } from '../../../../misc/is-muted-user-related';
 
 export default class extends Channel {
 	public readonly chName = 'antenna';
 	public static shouldShare = false;
-	public static requireCredential = false;
+	public static requireCredential = true;
 	private antennaId: string;
 
 	@autobind
 	public async init(params: any) {
+		if (typeof params.antennaId !== 'string') return false;
+		if (!this.user) return false;
+
 		this.antennaId = params.antennaId as string;
+
+		const antennaExists = await Antennas.findOne({
+			id: this.antennaId,
+			userId: this.user.id,
+		});
+
+		if (!antennaExists) return false;
 
 		// Subscribe stream
 		this.subscriber.on(`antennaStream:${this.antennaId}`, this.onEvent);
@@ -26,7 +36,7 @@ export default class extends Channel {
 
 			// 流れてきたNoteがミュートしているユーザーが関わるものだったら無視する
 			if (isMutedUserRelated(note, this.muting, false)) return;
-	
+
 			this.send('note', note);
 		} else {
 			this.send(type, body);
