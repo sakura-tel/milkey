028bb355fa1745ce2140971d8821890e4f155ea5
atsu1125 <atsuchan@atsuchan.page>
2026-07-15 13:27:56 +0900

    fix(backend): syslog typo

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -28,7 +28,7 @@ export default class Logger {
 
 		if (config.syslog) {
 			this.syslogClient = new SyslogPro.RFC5424({
-				applacationName: 'Groundpolis',
+				applicationName: 'Groundpolis',
 				timestamp: true,
 				encludeStructuredData: true,
 				color: true,
